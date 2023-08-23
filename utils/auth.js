d4a191c91a7a41305771a4c7ca83bc7d55ec830c
sdavi222 <130076128+sdavi222@users.noreply.github.com>
2023-08-22 21:59:55 -0400

    Added utils folder

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+// makes sure user is authorized to perform certain tasks (editing, deleting, commenting, etc.)
+
+const withAuth = (req, res, next) => {
+    if (!req.session.logged_in) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+};
+
+module.exports = withAuth;
---
+++ utils/helpers.js
@@ -0,0 +1,8 @@
+// formats date to MM/DD/YYYY
+
+module.exports = {
+    format_date: (date) => {
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()
+            }`;
+    },
+};
